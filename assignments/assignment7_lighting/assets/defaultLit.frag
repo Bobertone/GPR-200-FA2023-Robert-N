2b8ccbc3e108f321e328dce3e7726ea130fdba56
Bobertone <bobbyniewo@gmail.com>
2023-12-04 15:06:16 -0500

    Nearly finished assignment 7 - lighting need more imgui controls

--- assignments/assignment7_lighting/assets/defaultLit.frag
+++ assignments/assignment7_lighting/assets/defaultLit.frag
@@ -2,11 +2,50 @@
 out vec4 FragColor;
 
 in Surface{
-	vec2 UV;
+	vec2 UV; //Per-fragment interpolated UV
+	vec3 WorldPosition; //Per-fragment interpolated world position
+	vec3 WorldNormal; //Per-fragment interpolated world normal
 }fs_in;
+
+struct Light{
+	vec3 position;
+	vec3 color;
+};
+
+#define MAX_LIGHTS 4
+uniform Light _Lights[MAX_LIGHTS];
+
+uniform vec3 _camPosition;
+uniform int _lightCount;
+
+uniform float _ambientK; //Ambient coefficient (0-1)
+uniform float _diffuseK; //Diffuse coefficient (0-1)
+uniform float _specularK; //Specular coefficient (0-1)
+uniform float _shininess; //Shininess
 
 uniform sampler2D _Texture;
 
 void main(){
 	FragColor = texture(_Texture,fs_in.UV);
+	vec3 normal = normalize(fs_in.WorldNormal);
+	vec3 viewDirection = normalize(_camPosition - fs_in.WorldPosition);
+
+	vec3 ambient = vec3(0.0);
+	vec3 diffuse = vec3(0.0); 
+	vec3 specular = vec3(0.0);
+
+	for(int i = 0; i < _lightCount; i++){
+		vec3 lightDir = normalize(_Lights[i].position - fs_in.WorldPosition);
+		diffuse += _diffuseK * _Lights[i].color * max(dot(normal, lightDir), 0.0);
+	
+		vec3 reflectDirection = reflect(-lightDir, normal);
+		float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), _shininess);
+		specular += _specularK * spec * _Lights[i].color;
+
+		ambient += _ambientK * _Lights[i].color;
+	}
+	vec4 texColor = texture(_Texture, fs_in.UV);
+	vec3 resultColor = texColor.rgb *(ambient + diffuse + specular);
+	FragColor = vec4(resultColor, texColor.a);
+
 }
--- assignments/assignment7_lighting/main.cpp
+++ assignments/assignment7_lighting/main.cpp
@@ -21,11 +21,27 @@ void resetCamera(ew::Camera& camera, ew::CameraController& cameraController);
 int SCREEN_WIDTH = 1080;
 int SCREEN_HEIGHT = 720;
 
+const int MAX_LIGHTS = 4;
+int lightCount = MAX_LIGHTS;
+
 float prevTime;
 ew::Vec3 bgColor = ew::Vec3(0.1f);
 
 ew::Camera camera;
 ew::CameraController cameraController;
+
+struct Light {
+	ew::Vec3 position; //World space
+	ew::Vec3 color; //RGB
+};
+
+struct Material {
+	float ambientK; //Ambient coefficient (0-1)
+	float diffuseK; //Diffuse coefficient (0-1)
+	float specularK; //Specular coefficient (0-1)
+	float shininess; //Shininess
+};
+
 
 int main() {
 	printf("Initializing...");
@@ -61,11 +77,23 @@ int main() {
 	ew::Shader shader("assets/defaultLit.vert", "assets/defaultLit.frag");
 	unsigned int brickTexture = ew::loadTexture("assets/brick_color.jpg",GL_REPEAT,GL_LINEAR);
 
-	//Create cube
+	ew::Shader unlitShader("assets/unlit.vert", "assets/unlit.frag");
+
+	Material material {
+		material.ambientK = 1.0f, //Ambient coefficient (0-1)
+		material.diffuseK = 1.0f, //Diffuse coefficient (0-1)
+		material.specularK = 1.0f, //Specular coefficient (0-1)
+		material.shininess = 15.0f //Shininess
+	};
+
+	//Create Shapes
 	ew::Mesh cubeMesh(ew::createCube(1.0f));
 	ew::Mesh planeMesh(ew::createPlane(5.0f, 5.0f, 10));
 	ew::Mesh sphereMesh(ew::createSphere(0.5f, 64));
 	ew::Mesh cylinderMesh(ew::createCylinder(0.5f, 1.0f, 32));
+
+	//Create Material
+	Material mat;
 
 	//Initialize transforms
 	ew::Transform cubeTransform;
@@ -78,10 +106,31 @@ int main() {
 
 	resetCamera(camera,cameraController);
 
+	ew::Mesh lightSphereMesh = ew::createSphere(0.5f, 20);
+
+	ew::Transform lightTransforms[MAX_LIGHTS];
+
+	//Create lights
+	Light lights[MAX_LIGHTS]
+	{
+		lights[0].position = ew::Vec3(5.0f, 2.0f,7.0f),
+		lights[0].color = ew::Vec3(0.5f, 0.0f, 0.0f),
+
+		lights[1].position = ew::Vec3(5.0f, 2.0f,-7.0f),
+		lights[1].color = ew::Vec3(0.0f, 0.5f, 0.0f),
+
+		lights[2].position = ew::Vec3(-5.0f, 2.0f,0.0f),
+		lights[2].color = ew::Vec3(0.0f, 0.0f, 0.5f),
+
+		lights[3].position = ew::Vec3(0.0f, 6.0f,0.0f),
+		lights[3].color = ew::Vec3(0.15f, 0.15f, 0.15f)
+	};
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
 		float time = (float)glfwGetTime();
+
 		float deltaTime = time - prevTime;
 		prevTime = time;
 
@@ -97,6 +146,26 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, brickTexture);
 		shader.setInt("_Texture", 0);
 		shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
+		
+		shader.setFloat("_ambientK", material.ambientK);
+		shader.setFloat("_diffuseK", material.diffuseK);
+		shader.setFloat("_specularK", material.specularK);
+		shader.setFloat("_shininess", material.shininess);
+		shader.setVec3("_camPosition", camera.position);
+		shader.setInt("_lightCount", lightCount);
+		
+		//Set light pos & color
+		shader.setVec3("_Lights[0].position", lights[0].position);
+		shader.setVec3("_Lights[0].color", lights[0].color);
+
+		shader.setVec3("_Lights[1].position", lights[1].position);
+		shader.setVec3("_Lights[1].color", lights[1].color);
+
+		shader.setVec3("_Lights[2].position", lights[2].position);
+		shader.setVec3("_Lights[2].color", lights[2].color);
+
+		shader.setVec3("_Lights[3].position", lights[3].position);
+		shader.setVec3("_Lights[3].color", lights[3].color);
 
 		//Draw shapes
 		shader.setMat4("_Model", cubeTransform.getModelMatrix());
@@ -111,7 +180,17 @@ int main() {
 		shader.setMat4("_Model", cylinderTransform.getModelMatrix());
 		cylinderMesh.draw();
 
-		//TODO: Render point lights
+		//Render point lights
+		for(int i = 0; i < lightCount; i++)
+		{
+			unlitShader.use();
+			unlitShader.setVec3("_Color", lights[i].color);
+			unlitShader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
+			
+			lightTransforms[i].position = lights[i].position;
+			unlitShader.setMat4("_Model", lightTransforms[i].getModelMatrix());
+			lightSphereMesh.draw();
+		}
 
 		//Render UI
 		{
@@ -140,6 +219,21 @@ int main() {
 			}
 
 			ImGui::ColorEdit3("BG color", &bgColor.x);
+			
+			ImGui::SliderInt("Number of Lights", &lightCount, 0, MAX_LIGHTS);
+			/*
+			Material material{
+				material.ambientK = 1.0f, //Ambient coefficient (0-1)
+				material.diffuseK = 1.0f, //Diffuse coefficient (0-1)
+				material.specularK = 1.0f, //Specular coefficient (0-1)
+				material.shininess = 15.0f //Shininess
+			};
+			*/
+			ImGui::SliderFloat("Ambient", &material.ambientK, 0.0f, 1.0f);
+			ImGui::SliderFloat("Diffuse", &material.diffuseK, 0.0f, 1.0f);
+			ImGui::SliderFloat("Specular", &material.specularK, 0.0f, 1.0f);
+			ImGui::SliderFloat("Shininess", &material.shininess, 0.0f, 255.0f);
+
 			ImGui::End();
 			
 			ImGui::Render();
